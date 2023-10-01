590648b426ff522aec48eb2925fe93770e3088c8
Daniyal Idrees <>
2023-10-01 21:15:33 +0200

    domain model properties set to non nullable

--- domain/src/main/kotlin/com/example/domain/model/Book.kt
+++ domain/src/main/kotlin/com/example/domain/model/Book.kt
@@ -1,7 +1,7 @@
 package com.example.domain.model
 
 data class Book(
-    val title: String? = null,
-    val authors: List<String>? = null,
+    val title: String,
+    val authors: List<String>,
     val imageUrl: String? = null,
 )
