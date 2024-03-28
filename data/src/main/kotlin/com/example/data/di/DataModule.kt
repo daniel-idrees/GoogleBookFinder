3cde7a0f5911d51a6d5a7c1eff44b387daba8be4
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-03-28 13:38:49 +0100

    Use case dependencies separated

--- data/src/main/kotlin/com/example/data/di/DataModule.kt
+++ data/src/main/kotlin/com/example/data/di/DataModule.kt
@@ -1,25 +1,19 @@
-package com.example.googlebookfinder.di
+package com.example.data.di
 
 import com.example.data.repository.BookRepositoryImpl
 import com.example.domain.repository.BookRepository
-import com.example.domain.usecase.GetBookListUseCase
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataModule {
-    @Provides
+internal interface DataModule {
+    @Binds
     @Singleton
     fun providesBookRepository(
         impl: BookRepositoryImpl,
-    ): BookRepository = impl
-
-    @Provides
-    fun providesUseCase(
-        bookRepository: BookRepository,
-    ): GetBookListUseCase = GetBookListUseCase(bookRepository)
+    ): BookRepository
 }
---
+++ domain/src/main/kotlin/com/example/domain/UseCaseModule.kt
@@ -0,0 +1,20 @@
+package com.example.domain
+
+import com.example.domain.repository.BookRepository
+import com.example.domain.usecase.GetBookListUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+internal class UseCaseModule {
+
+    @Provides
+    @ViewModelScoped
+    fun providesUseCase(
+        bookRepository: BookRepository,
+    ): GetBookListUseCase = GetBookListUseCase(bookRepository)
+}
