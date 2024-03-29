bbe17d86400c8fb409ed250cadf816879a7568ff
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-03-29 22:02:22 +0100

    rethrow cancellation exception in run catching

--- data/src/main/kotlin/com/example/data/repository/BookRepositoryImpl.kt
+++ data/src/main/kotlin/com/example/data/repository/BookRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.data.repository
 
+import com.example.core.util.runSuspendCatching
 import com.example.data.dto.mapper.toBookList
 import com.example.data.network.BookFinderService
 import com.example.domain.model.BookDataResult
@@ -11,13 +12,15 @@ internal class BookRepositoryImpl @Inject constructor(
     private val bookFinderService: BookFinderService,
 ) : BookRepository {
     override suspend fun getBooks(searchQuery: String): BookDataResult =
-        runCatching {
+        runSuspendCatching {
             val response = bookFinderService.getBookList(searchQuery).toBookList()
             if (response.isNotEmpty()) {
                 BookDataResult.Success(response)
             } else {
                 BookDataResult.Empty
             }
+        }.onFailure {
+            // log
         }.getOrElse {
             getErrorResult(it)
         }
