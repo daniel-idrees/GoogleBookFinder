7f8da2be8133d60ef39e9dbac1d3c56446177222
Daniyal Idrees <enggdaniyal@hotmail.com>
2024-03-31 22:15:04 +0200

    use case data type replaced with flow and unit test updated

--- domain/src/main/kotlin/com/example/domain/usecase/GetBookListUseCase.kt
+++ domain/src/main/kotlin/com/example/domain/usecase/GetBookListUseCase.kt
@@ -2,12 +2,14 @@ package com.example.domain.usecase
 
 import com.example.domain.model.BookDataResult
 import com.example.domain.repository.BookRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class GetBookListUseCase @Inject constructor(
     private val bookRepository: BookRepository,
 ) {
-    suspend fun get(
+    suspend operator fun invoke(
         searchQuery: String,
-    ): BookDataResult = bookRepository.getBooks(searchQuery)
+    ): Flow<BookDataResult> = flowOf(bookRepository.getBooks(searchQuery))
 }
--- domain/src/test/kotlin/com/example/domain/usecase/GetBookListUseCaseTest.kt
+++ domain/src/test/kotlin/com/example/domain/usecase/GetBookListUseCaseTest.kt
@@ -1,10 +1,11 @@
 package com.example.domain.usecase
 
+import app.cash.turbine.test
 import com.example.domain.model.Book
 import com.example.domain.model.BookDataResult
 import com.example.domain.repository.BookRepository
-import io.kotest.common.runBlocking
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
@@ -14,37 +15,57 @@ import org.mockito.kotlin.whenever
 
 internal class GetBookListUseCaseTest {
     private val bookRepository: BookRepository = mock()
-    private val subject = GetBookListUseCase(bookRepository)
+    private val subject by lazy {
+        GetBookListUseCase(bookRepository)
+    }
 
     private val fakeQuery = "fakeQuery"
 
     @Test
-    fun `get should return book list if the repository returns the book list`() = runBlocking {
+    fun `get should return book list if the repository returns the book list`() = runTest {
+        // when
         val bookList = listOf(Book("", listOf(), ""))
         whenever(bookRepository.getBooks(fakeQuery)) doReturn BookDataResult.Success(bookList)
-        val result = subject.get(fakeQuery)
-        verify(bookRepository).getBooks(fakeQuery)
-        verifyNoMoreInteractions(bookRepository)
-        result shouldBe BookDataResult.Success(bookList)
+
+        // then
+        subject(fakeQuery).test {
+            verify(bookRepository).getBooks(fakeQuery)
+            verifyNoMoreInteractions(bookRepository)
+            val result = awaitItem()
+            result shouldBe BookDataResult.Success(bookList)
+            cancelAndIgnoreRemainingEvents()
+        }
     }
 
 
     @Test
-    fun `get should return error if the repository returns error`() = runBlocking {
-        whenever(bookRepository.getBooks(fakeQuery)) doReturn BookDataResult.Error("error")
-        val result = subject.get(fakeQuery)
-        verify(bookRepository).getBooks(fakeQuery)
-        verifyNoMoreInteractions(bookRepository)
-        result shouldBe BookDataResult.Error("error")
+    fun `get should return error if the repository returns error`() = runTest {
+        // when
+        whenever(bookRepository.getBooks(fakeQuery)) doReturn BookDataResult.Error
+
+        // then
+        subject(fakeQuery).test {
+            verify(bookRepository).getBooks(fakeQuery)
+            verifyNoMoreInteractions(bookRepository)
+            val result = awaitItem()
+            result shouldBe BookDataResult.Error
+            cancelAndIgnoreRemainingEvents()
+        }
     }
 
 
     @Test
-    fun `get should return empty if the repository returns empty`() = runBlocking {
+    fun `get should return empty if the repository returns empty`() = runTest {
+        // when
         whenever(bookRepository.getBooks(fakeQuery)) doReturn BookDataResult.Empty
-        val result = subject.get(fakeQuery)
-        verify(bookRepository).getBooks(fakeQuery)
-        verifyNoMoreInteractions(bookRepository)
-        result shouldBe BookDataResult.Empty
+        
+        // then
+        subject(fakeQuery).test {
+            verify(bookRepository).getBooks(fakeQuery)
+            verifyNoMoreInteractions(bookRepository)
+            val result = awaitItem()
+            result shouldBe BookDataResult.Empty
+            cancelAndIgnoreRemainingEvents()
+        }
     }
 }
